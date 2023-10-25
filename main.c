bb2aa838a424ae5804be3c83f1de047c3316a2f4
Charles-Antoine <charto.bta@gmail.com>
2023-10-25 17:08:30 +0200

    v1.0

--- main.c
+++ main.c
@@ -1,18 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Number{
-    int value;
-    struct Number* next;
-}Number;
-
-void add_item(Number ** list, int value){
-    Number * item = malloc(sizeof(Number));
-    item->value = value;
-    item->next = *list;
-    *list = item;
-}
-
 int get_number(int number, int divider){
     return number/divider;
 }
@@ -25,34 +13,21 @@ int bigger_divider(int number){
     return divider/10;
 }
 
-void separate_number(int number, Number *list){
+int sum_number(int number){
     int divider = bigger_divider(number);
-    while (divider > 1){
-        add_item(list, get_number(number,divider));
+    int sum = 0;
+    while (divider > 0){
+        sum += get_number(number,divider);
         number %= divider;
         divider /= 10;
     }
-}
-
-int sum_numbers(Number *list){
-    if(list == NULL) return 0;
-    int sum = 0;
-    while (list->next != NULL){
-        sum += list->value;
-        list = list->next;
-    }
-    sum += list->value;
     return sum;
 }
 
 int main() {
-    int number = 34;
-    int divider= 1;
-    Number *list = NULL;
-//    scanf("%d",&number);
-//    printf("%d", get_number(number,divider));
-    separate_number(number,list);
-//    int sum = sum_numbers(list);
-//    printf("%d",sum);
+    int number;
+    printf("Enter number: ");
+    scanf("%d",&number);
+    printf("%d",sum_number(number));
     return 0;
 }
